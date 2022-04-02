ab8b0635b9857d7291efb3317173aa0fb4eb8458
thenderson80 <102861698+thenderson80@users.noreply.github.com>
2022-04-02 11:22:24 -0400

    Add files via upload

---
+++ P1HW1_HendersonTodd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+print("todd henderson")
+print("2944 Rosemeade Drive")
+print("Fayetteville, NC 28306")
+print("")
+""""""
+# get First Last name
+FirstName = input("Enter first name here: ")
+LastName = input("Enter last name here: ")
+# Combine Both Names
+name = FirstName + " " + LastName
+# say hi
+print("Hello,", name)
+print("")
+# ask user to enter two numbers
+num1 = int( input("Enter first number here: "))
+num2 = int( input("Enter second number here: "))
+total = num1 + num2
+#  display results
+print("Result", total)
+input()
+
+print("  NO PARKING")
+print("02:00 - 06:00", end="\n")
---
+++ P1HW2_HendersonTodd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" This project is for CTI-110 (Web, Program, & Database Foundation) entitled P1HW2 Pizza
+31 March 2022
+CTI-110 P1hW2 - Pizza Order
+Todd Henderson"""
+# Get total number of students
+num1 = int (input("How many students do you want to order pizza for?"" "))
+print("")
+print("----Pizza Order--------")
+num2 = int (input("Number of Students     :"" "))
+num3 = int (input("Number of Slices Needed:"" "))
+# Total Number of Pizzas
+total = num3 // 6
+print("Pizzas Needed :", total)
+print("________________________")
