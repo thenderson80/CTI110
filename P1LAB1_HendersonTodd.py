0336c7d269b818d833b88169ecd12033d72d35f8
thenderson80 <102861698+thenderson80@users.noreply.github.com>
2022-04-27 12:11:03 -0400

    Add files via upload

--- P1LAB1_HendersonTodd.py
+++ P1LAB1_HendersonTodd.py
@@ -1,2 +1,10 @@
-print("  NO PARKING")
-print("2:00 - 6:00 a.m.")
+# CTI-110
+# P1LAB1 - Using ZyBooks Lab Basics
+# Todd Henderson
+# 8 April 2022
+
+
+userNum = int(input("Enter the users number: "))
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared, end=' ')       # Output formatting issue here; fix it when instructed
