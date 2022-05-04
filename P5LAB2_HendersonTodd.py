e2ddd8e66e51bf05799aeddfdefb6749904f0dc2
thenderson80 <102861698+thenderson80@users.noreply.github.com>
2022-05-04 11:12:30 -0400

    Add files via upload

---
+++ P5LAB2_HendersonTodd.py
@@ -0,0 +1,16 @@
+# CTI-110
+# P5LAB2 - Step Counter
+# Todd Henderson
+# 10 May 2022
+
+def miles(user_steps):
+	return ("{:.2f}".format(miles))
+
+user_steps = int(input("How many steps did you take? "))
+miles = user_steps / 2000
+print(f"You walked{miles: .2f} miles")
+
+
+
+
+
